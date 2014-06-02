d103838ea31936f99b17d1d0bbde8a37a0a8161f
enzo - Eduardo Garcia <enzo@anexusit.com>
2014-06-02 08:44:42 -0500

    Enable render function

--- SpressGoogleAnalytics.php
+++ SpressGoogleAnalytics.php
@@ -5,21 +5,25 @@ namespace SpressPlugins\SpressGoogleAnalytics;
 use Yosymfony\Spress\Plugin\Plugin;
 use Yosymfony\Spress\Plugin\EventSubscriber;
 use Yosymfony\Spress\Plugin\Event\EnviromentEvent;
+use Yosymfony\Spress\Plugin\Event\RenderEvent;
 
 class SpressGoogleAnalytics extends Plugin
 {
     public function initialize(EventSubscriber $subscriber)
     {
-      die('here');
       $subscriber->addEventListener('spress.after_render', 'onAfterRender');
     }
 
     public function onAfterRender(RenderEvent $event)
     {
 
-      $config = $event->getConfigRepository();
-      // Get the content without Front-matter (string):
-      $content = $event->getContent();
-      print_r($content);
+      $payload = $event->getPayload();
+
+      // Site Configuration
+      //$payload['site']);
+
+      // Page Content
+      // $payload['page']['content']);
+
     }
 }
